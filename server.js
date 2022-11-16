1389e3a714fd8cf0b81d3f18b79a9b307cdd062c
Roger gallardo <rogergallardofs1@gmail.com>
2022-11-16 00:05:32 -0500

    third commit

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const drinks = require('./models/drinks')
 
 
 app.get("/" , (req, res) => {
     res.send('Welcome to the gitpub App')
 })
 
+app.get('/drinks', (req,res)=>{
+    res.render('drinks_index.ejs', {
+    });
+});
 
 app.listen(port, () => {
     console.log('listening')
