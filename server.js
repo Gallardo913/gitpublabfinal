f373516449297c67ac530d28cd714651d7189be6
Roger gallardo <rogergallardofs1@gmail.com>
2022-11-17 23:21:11 -0500

    last commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const drinks = require('./models/drinks');
-
+const food = require('./models/food');
 
 app.get("/" , (req, res) => {
     res.send('Welcome to the gitpub App')
@@ -19,6 +19,12 @@ app.get('/drinks/:id', (req, res) => {
         drink: drinks[req.params.id],
     }); 
 }); 
+
+app.get('/food/:id', (req, res) => {
+    res.render('food_show.ejs', {
+        food: food[req.params.id],
+    });
+});
 
 app.listen(port, () => {
     console.log('listening')
